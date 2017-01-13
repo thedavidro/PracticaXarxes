c537dee9a8e1d161135e195d2448de57d45b0683
Daniel Bruna <zombiebust3r@gmail.com>
2017-01-13 19:18:53 +0100

    MIXED PROJECTS WITH XML
    Mixed both projects

---
+++ src/Server_Client/Client-Server/XML_Includes.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <string>
+
+#include "XML\rapidxml.hpp"
+#include "XML\rapidxml_iterators.hpp"
+#include "XML\rapidxml_print.hpp"
+#include "XML\rapidxml_utils.hpp"
+
+#include "Resource.hpp"
+
+using namespace std;
---
+++ src/Server_Client/Client-Server/main.cpp
@@ -0,0 +1,12 @@
+#include "XML.hpp"
+
+
+using namespace std;
+
+void main() {
+	CreateNewNode("HELLO PAULA", 600, "10101");
+	CreateNewNode("HOW YOU DOING", 600, "10101");
+
+
+	//cout << "hello" << endl;
+}
